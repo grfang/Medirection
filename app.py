3f198bb22939d18dbd464eee7f0db560ff4053de
Gia Fang <giafang16@gmail.com>
2024-01-27 15:36:40 -0800

    added text transcription and translation api
    Co-authored-by: Sharon Ma <sharonm6@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 # backend/app.py
 from flask import Flask, jsonify
+import json
 import psycopg2
 import random
 import string
+from datetime import datetime
+
+from dotenv import load_dotenv
+from deepgram import DeepgramClient, DeepgramClientOptions, PrerecordedOptions
+from google.cloud import translate
+
+load_dotenv()
 
 app = Flask(__name__)
+
+PROJECT_ID = "medirection"
+PARENT = f"projects/{PROJECT_ID}"
+CLIENT = translate.TranslationServiceClient()
 
 # Replace these variables with your PostgreSQL connection details
 DB_USER = 'postgres'
@@ -25,20 +37,17 @@ conn = psycopg2.connect(
 # Create a cursor to interact with the database
 cursor = conn.cursor()
 
-
 # cursor.close()
 # conn.close()
 
-# phone_number = '+812347983246'
-# firstname = 'miffy'
-# lastname = 'bunny'
-# role = 'patient'
-
-
 @app.route('/signup', methods=['POST'])
-def signup(phone_number, firstname, lastname, role):
+def signup(phone_number, firstname, lastname, role, language):
+    with open('lang_codes.json', 'r') as f:
+        lang_codes = json.loads(f.read())
+    print('lang_codes', lang_codes)
+
     user_id = ''.join(random.choices(string.ascii_uppercase + string.digits, k=4))
-    cursor.execute("INSERT INTO users(id, phonenumber, firstName, lastName, role) VALUES('%s', '%s', '%s', '%s', '%s') RETURNING id;" % (user_id, phone_number, firstname, lastname, role))
+    cursor.execute("INSERT INTO users(id, phonenumber, firstName, lastName, role, language) VALUES('%s', '%s', '%s', '%s', '%s', '%s') RETURNING id;" % (user_id, phone_number, firstname, lastname, role, lang_codes[language]))
     conn.commit()
     user_id = cursor.fetchone()
     if user_id:
@@ -50,6 +59,7 @@ def signup(phone_number, firstname, lastname, role):
 def login(phone_number):
     cursor.execute("SELECT id FROM users WHERE phonenumber = '%s';" % (phone_number))
     user = cursor.fetchone() # user corresponding to phone number
+    
     if user:
         return jsonify({'user_id': user[0]})
     else:
@@ -70,6 +80,73 @@ def get_dashboard(user_id):
         return jsonify({'chatpage_info': blurb_list})
     else:
         return jsonify({'chatpage_info': None})
+
+@app.route('/chatroom', methods=['GET'])
+def get_messages(channelid):
+    cursor.execute("SELECT audiourl, transcription, translation, senderid, timestamp FROM messages WHERE channelid = '%s' SORT BY timestamp;" % (channelid))
+    chatroom_messages = cursor.fetchall()
     
-# if __name__ == '__main__':
-#     app.run()
+    if chatroom_messages:
+        return jsonify({'messages': [{'audiourl': chatroom_messages[idx][0], 'transcription': chatroom_messages[idx][1], 'translation': chatroom_messages[idx][2], 'senderid': chatroom_messages[idx][3], 'timestamp': chatroom_messages[idx][4]} for idx in range(len(chatroom_messages))]})
+    else:
+        return jsonify({'messages': None})
+
+def get_transcription(audio, sender_id):
+    # get lang from sender_id
+    cursor.execute("SELECT language FROM users WHERE id = '%s';" % (sender_id))
+    lang = cursor.fetchone()[0]
+    
+    try:
+        # call deepgram to get transscription
+        deepgram = DeepgramClient()
+        options = PrerecordedOptions(
+            model="nova",
+            smart_format=True,
+            summarize="v2",
+            language=lang
+        )
+        url_response = deepgram.listen.prerecorded.v("1").transcribe_url(
+            {"url": audio}, options
+        )
+        return url_response.results.channels[0].alternatives[0].transcript
+    except Exception as e:
+        print(f"Exception: {e}")
+    return None
+
+def translate_text(text: str, target_language_code: str, source_language_code: str) -> translate.Translation:
+    response = CLIENT.translate_text(
+        parent=PARENT,
+        contents=[text],
+        target_language_code=target_language_code,
+        source_language_code=source_language_code
+    )
+    return response.translations[0]
+
+def get_translation(transcription, doctor_id, sender_id):
+    # get lang from doctor_id
+    cursor.execute("SELECT language FROM users WHERE id = '%s';" % (doctor_id))
+    target_lang = cursor.fetchone()[0]
+    cursor.execute("SELECT language FROM users WHERE id = '%s';" % (sender_id))
+    src_lang = cursor.fetchone()[0]
+    
+    if target_lang == src_lang:
+        return transcription
+
+    translation = translate_text(transcription, target_lang, src_lang)
+    return translation.translated_text
+
+@app.route('/send', methods=['POST'])
+def send_message(audio_url, channel_id, doctor_id, sender_id):
+    transcription = get_transcription(audio_url, sender_id).replace("'", r"\'")
+    translation = get_translation(transcription, doctor_id, sender_id).replace("'", r"\'")
+    print(transcription)
+    print(translation)
+    message_id = ''.join(random.choices(string.ascii_uppercase + string.digits, k=4))
+    timestamp = str(int(datetime.now().timestamp()))
+    query = "INSERT INTO messages(messageid, channelid, timestamp, senderid, audiourl, transcription, translation) VALUES(%s, %s, %s, %s, %s, %s, %s)"
+    cursor.execute(query, (message_id, channel_id, timestamp, sender_id, audio_url, transcription, translation))
+    conn.commit()
+    return jsonify({'transcription': transcription, 'translation': translation})
+
+if __name__ == '__main__':
+    app.run()
