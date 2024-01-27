d1e04a17d1fc7a806b7327f065f7bd9db3557873
Gia Fang <giafang16@gmail.com>
2024-01-27 11:11:00 -0800

    added login and signup, frontend also gets ids now
    Co-authored-by: Sharon Ma <sharonm6@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 # backend/app.py
 from flask import Flask, jsonify
 import psycopg2
+import random
+import string
 
 app = Flask(__name__)
 
@@ -23,27 +25,51 @@ conn = psycopg2.connect(
 # Create a cursor to interact with the database
 cursor = conn.cursor()
 
+
 # cursor.close()
 # conn.close()
 
-#Define a route to fetch users from the database
-@app.route('/dashboard', methods=['GET'])
-def get_users(phone_number):
+# phone_number = '+812347983246'
+# firstname = 'miffy'
+# lastname = 'bunny'
+# role = 'patient'
+
+
+@app.route('/signup', methods=['POST'])
+def signup(phone_number, firstname, lastname, role):
+    user_id = ''.join(random.choices(string.ascii_uppercase + string.digits, k=4))
+    cursor.execute("INSERT INTO users(id, phonenumber, firstName, lastName, role) VALUES('%s', '%s', '%s', '%s', '%s') RETURNING id;" % (user_id, phone_number, firstname, lastname, role))
+    conn.commit()
+    user_id = cursor.fetchone()
+    if user_id:
+        return jsonify({'user_id': user_id})
+    else:
+        return jsonify({'user_id': None})
+    
+@app.route('/login', methods=['GET'])
+def login(phone_number):
     cursor.execute("SELECT id FROM users WHERE phonenumber = '%s';" % (phone_number))
     user = cursor.fetchone() # user corresponding to phone number
-    cursor.execute("SELECT channelid, doctorid, summary FROM channels WHERE patientid = '%s';" % (user[0]))
-    chatpage_info = cursor.fetchall() # channel[0] = channelid and channel[1] = doctorid and channel[2] = summary
+    if user:
+        return jsonify({'user_id': user[0]})
+    else:
+        return jsonify({'user_id': None})
+
+@app.route('/dashboard', methods=['GET'])
+def get_dashboard(user_id):
+    cursor.execute("SELECT channelid, doctorid, summary FROM channels WHERE user_id = '%s';" % (user_id))
+    chatpage_info = cursor.fetchall() # chatpage_info[0] = channelid, chatpage_info[1] = doctorid, chatpage_info[2] = summary
     for idx in range(len(chatpage_info)):
         cursor.execute("SELECT firstname, lastname FROM users WHERE id = '%s';" % (chatpage_info[idx][1]))
         doctorTuple = cursor.fetchone()
         doctorName = doctorTuple[0] + " " + doctorTuple[1]
-        chatpage_info[idx] = (chatpage_info[idx][2], doctorName)
+        chatpage_info[idx] = (doctorName, chatpage_info[idx][1], chatpage_info[idx][0], chatpage_info[idx][2])
     
     if chatpage_info:
-        blurb_list = [{'summary': chatpage_info[idx][0], 'doctor': chatpage_info[idx][1]} for idx in range(len(chatpage_info))]
+        blurb_list = [{'doctorname': chatpage_info[idx][0], 'doctorid': chatpage_info[idx][1], 'chatpageid': chatpage_info[idx][2], 'summary': chatpage_info[idx][3]} for idx in range(len(chatpage_info))]
         return jsonify({'chatpage_info': blurb_list})
     else:
         return jsonify({'chatpage_info': None})
     
-if __name__ == '__main__':
-    app.run()
+# if __name__ == '__main__':
+#     app.run()
