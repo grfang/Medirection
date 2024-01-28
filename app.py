8f62428eac3c2b3c8803433cce70155a1575f005
Gia Fang <giafang16@gmail.com>
2024-01-27 23:27:18 -0800

    finished todolist, final commit before connecting with frontend (hopefully)

--- app.py
+++ app.py
@@ -274,16 +274,44 @@ def summarize(convo, doctor_lang_name, patient_lang_name):
         ],
         model="gpt-3.5-turbo",
     )
-    print("chat_completion: ", chat_completion)
+    
+    return chat_completion.choices[0].message.content
 
-def generate_todos(channel_id):
-    pass
+def generate_todos(convo, doctor_lang_name, patient_lang_name):
+    chat_completion = GPT_CLIENT.chat.completions.create(
+        messages=[
+            {"role": "system", "content": "You are a helper at a hospital whose job is to provide concise  lists of action items after conversations during appointments and rephrase them in simple terms that someone in middle school could understand. Please read the following chat logs between a doctor and a patient. The messages from the doctor will be in " + doctor_lang_name + " and the messages patient will be in " + patient_lang_name + ", switching back and forth, separated by new lines. Identify up to 5 short phrases of action items in english. Format your response as comma separated list of action items."},
+            {
+                "role": "user",
+                "content": "Here is the chat log: Avez-vous eu des réactions au nouveau médicament?\n Nada demasiado severo, solo un par de erupciones.\n Cela a-t-il aidé à améliorer votre qualité de sommeil?\n Sí, definitivamente puedo ver una diferencia.\n"
+                # "content": "Here is the chat log: Have you had any reactions to the new medication?\n Nothing too severe, just a couple of rashes.\n Has it helped improve your sleep quality?\n Yes, I can definitely see a difference.\n"
+            },
+            {
+                "role": "assistant",
+                "content": "Monitor rash, Monitor sleep condition, Take medication"
+            },
+            {
+                "role": "user",
+                "content": "Here is the chat log: Tengo un dolor muy agudo en la oreja. Hay una descarga de mal olor proveniente de ella.\n Cest un cas classique dotite. Je vais prescrire des antibiotiques. Prenez-les deux fois par jour pendant sept jours et votre infection devrait s'améliorer.\n"
+                # "content": "Here is the chat log: I have a very sharp pain in my ear. There is bad smelling discharge coming from it.\n This is classic case of an ear infection. I will prescribe some antibiotics. Take them twice a day for seven days and your infection should be better.\n"
+            },
+            {
+                "role": "assistant",
+                "content": "Take antibiotics twice a day for seven days"
+            },
+            {
+                "role": "user",
+                "content": "Here is the chat log: " + convo
+            }
+        ],
+        model="gpt-3.5-turbo",
+    )
+    return chat_completion.choices[0].message.content.split(',')
 
 @app.route('/close', methods=['POST'])
 def close(channel_id):
     query = "UPDATE channels SET status = 'closed' WHERE channelid = %s;"
     cursor.execute(query, (channel_id))
-    conn.commit()
 
     query = "SELECT transcription FROM messages WHERE channelid = %s ORDER BY timestamp ASC;"
     cursor.execute(query, (channel_id))
@@ -310,10 +338,19 @@ def close(channel_id):
     patient_language_code = cursor.fetchone()
     patient_lang_name= codes_to_lang[patient_language_code[0]]
 
-    summary = summarize(convo, doctor_lang_name, patient_lang_name)
-    todoList = generate_todos(channel_id)
+    summary = summarize(convo, doctor_lang_name, patient_lang_name) # string
+    todoList = generate_todos(convo, doctor_lang_name, patient_lang_name) # list of strings
+    
+    query = "UPDATE channels SET summary = %s WHERE channelid = %s;"
+    cursor.execute(query, (summary, channel_id))
+    
+    query = "INSERT INTO todos(channelid, userid, doctorid, actions) VALUES(%s, %s, %s, %s)"
+    cursor.execute(query, (channel_id, [1], chatters[0], todoList))
+    conn.commit()
+    
+    return jsonify({'exit_code': 0})
 
-@app.route('/create', methods=['GET', 'POST'])
+@app.route('/create', methods=['GET'])
 def create(doctor_id, phone_number):
     query = "SELECT id, firstname, lastname FROM users WHERE phonenumber = %s;"
     cursor.execute(query, (phone_number))
@@ -351,8 +388,6 @@ def get_action_plans(user_id):
         doctorTuple = cursor.fetchone()
         doctorName = doctorTuple[0] + " " + doctorTuple[1]
         todo_info[idx] = (doctorName, todo_info[idx][1])
-    
-    print({'todos': [{'doctor_name': todo_info[idx][0], 'todo_list': todo_info[idx][1]}] for idx in range(len(todo_info))})
     
     if todo_info:
         return jsonify({'todos': [{'doctor_name': todo_info[idx][0], 'todo_list': todo_info[idx][1]}] for idx in range(len(todo_info))})
